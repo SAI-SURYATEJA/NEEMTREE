6fef9c87f7c1202efed990b9aaa85541dd31fb1a
SAI-SURYATEJA <122626275+SAI-SURYATEJA@users.noreply.github.com>
2023-12-21 14:19:20 +0300

    fix: js logic fix

--- main.js
+++ main.js
@@ -1,85 +1,220 @@
 
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const navList = document.querySelector('.nav-list');
+// const navToggle = document.querySelector('.mobile-nav-toggle');
+// const navList = document.querySelector('.nav-list');
 
-navToggle.addEventListener('click', function () {
-  const expanded = navToggle.getAttribute('aria-expanded') === 'true';
-  navToggle.setAttribute('aria-expanded', !expanded);
-  navList.setAttribute('data-visible', !expanded);
+// navToggle.addEventListener('click', function () {
+//   const expanded = navToggle.getAttribute('aria-expanded') === 'true';
+//   navToggle.setAttribute('aria-expanded', !expanded);
+//   navList.setAttribute('data-visible', !expanded);
   
   
-  if (!expanded) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
+//   if (!expanded) {
+//     document.body.style.overflow = 'hidden';
+//   } else {
+//     document.body.style.overflow = '';
+//   }
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+// const universitiesSection = document.querySelector('.universities');
+
+
+// const scrollSpeed = 4; 
+
+// function scrollUniversities() {
+//   universitiesSection.scrollLeft += scrollSpeed;
+  
+  
+//   if (universitiesSection.scrollLeft >= universitiesSection.scrollWidth - universitiesSection.offsetWidth) {
+//     const imgElements = universitiesSection.querySelectorAll('img');
+//     imgElements.forEach(img => {
+//       const cloneImg = img.cloneNode(true);
+//       universitiesSection.appendChild(cloneImg);
+//     });
+//   }
+// }
+
+// const scrollInterval = setInterval(scrollUniversities, 50); 
+
+
+// universitiesSection.addEventListener('mouseenter', function () {
+//   clearInterval(scrollInterval);
+// });
+
+
+// universitiesSection.addEventListener('mouseleave', function () {
+//   scrollInterval = setInterval(scrollUniversities, 50);
+// });
+// });
+
+// $(document).ready(function(){
+// $(".front_arrow").click(function(){
+//   $("#testimonial-carousel").carousel("next");
+// });
+
+// $(".back_arrow").click(function(){
+//   $("#testimonial-carousel").carousel("prev");
+// });
+
+// var cardsInSlide = 3;
+// var totalCards = $(".carousel-cards").length;
+
+// $("#testimonial-carousel").on("slide.bs.carousel", function (event) {
+//   var index = $(event.relatedTarget).index();
+//   var visibleCards = cardsInSlide;
+
+//   if (index + cardsInSlide > totalCards) {
+//     visibleCards = totalCards - index;
+//   }
+
+//   $(".carousel-cards").removeClass("active");
+//   $(".carousel-cards").slice(index, index + visibleCards).addClass("active");
+// });
+// });
+
+// // function openPopup() {
+// //   document.getElementById('form').style.display = 'flex';
+// // }
+// // function closePopup() {
+// //   document.getElementById('form').style.display = 'none';
+// // }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var sets = document.querySelectorAll('.content-set');
+//   var nextButton = document.getElementById('next');
+//   var bookButton = document.getElementById('book');
+//   var closeButton = document.querySelector('.close-btn');
+//   var tickContainer = document.getElementById('tickContainer'); // Ensure this ID matches your tick container in HTML
+//   var currentSet = 0;
+
+//   function openPopup() {
+//       document.getElementById('popupForm').style.display = 'block';
+//       resetForm();
+//   }
+
+//   window.openPopup = openPopup; // Make it globally accessible if referenced by 'onclick' attributes
+
+//   function closePopup() {
+//       document.getElementById('popupForm').style.display = 'none';
+//   }
+
+//   function toggleContent() {
+//       sets[currentSet].style.display = 'none';
+//       currentSet++;
+//       sets[currentSet].style.display = 'block';
+      
+//       if (currentSet === sets.length - 1) {
+//           nextButton.style.display = 'none';
+//           bookButton.style.display = 'block';
+//       }
+//   }
+
+//   window.toggleContent = toggleContent;
+//   function submitForm() {
+//     // Hide form container
+//     document.getElementById('formContainer').style.display = 'none';
+
+//     // Show tick and hide popup after 2 seconds
+//     tickContainer.style.display = 'flex';
+
+//     setTimeout(function() {
+//         tickContainer.style.display = 'none'; // Hide the tick container
+//         resetForm();
+//         closePopup(); // This should come last after the tick disappears
+//     }, 2000);
+// }
+
+//   window.submitForm = submitForm;
+
+//   function resetForm() {
+//       document.getElementById('consultationForm').reset();
+//       sets.forEach(function(set, index) {
+//           set.style.display = index === 0 ? 'block' : 'none';
+//       });
+//       currentSet = 0;
+      
+//       // Reset buttons display
+//       nextButton.style.display = 'block';
+//       bookButton.style.display = 'none';
+      
+//       // Ensure the form container is displayed when resetting
+//       document.getElementById('formContainer').style.display = 'block';
+//   }
+
+//   window.resetForm = resetForm;
+
+//   closeButton.addEventListener('click', function() {
+//       closePopup();
+//   });
+// });
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.mobile-nav-toggle');
+  const navList = document.querySelector('.nav-list');
+
+  navToggle.addEventListener('click', function () {
+    const expanded = navToggle.getAttribute('aria-expanded') === 'true';
+    navToggle.setAttribute('aria-expanded', !expanded);
+    navList.setAttribute('data-visible', !expanded);
+
+    if (!expanded) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  });
+
+  const universitiesSection = document.querySelector('.universities');
+  const scrollSpeed = 4;
+
+  function scrollUniversities() {
+    universitiesSection.scrollLeft += scrollSpeed;
+
+    if (universitiesSection.scrollLeft >= universitiesSection.scrollWidth - universitiesSection.offsetWidth) {
+      const imgElements = universitiesSection.querySelectorAll('img');
+      imgElements.forEach(img => {
+        const cloneImg = img.cloneNode(true);
+        universitiesSection.appendChild(cloneImg);
+      });
+    }
   }
+
+  let scrollInterval = setInterval(scrollUniversities, 50);
+
+  universitiesSection.addEventListener('mouseenter', function () {
+    clearInterval(scrollInterval);
+  });
+
+  universitiesSection.addEventListener('mouseleave', function () {
+    scrollInterval = setInterval(scrollUniversities, 50);
+  });
+});
+
+$(document).ready(function () {
+  $(".front_arrow").click(function () {
+    $("#testimonial-carousel").carousel("next");
+  });
+
+  $(".back_arrow").click(function () {
+    $("#testimonial-carousel").carousel("prev");
+  });
+
+  var cardsInSlide = 3;
+  var totalCards = $(".carousel-cards").length;
+
+  $("#testimonial-carousel").on("slide.bs.carousel", function (event) {
+    var index = $(event.relatedTarget).index();
+    var visibleCards = cardsInSlide;
+
+    if (index + cardsInSlide > totalCards) {
+      visibleCards = totalCards - index;
+    }
+
+    $(".carousel-cards").removeClass("active");
+    $(".carousel-cards").slice(index, index + visibleCards).addClass("active");
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-const universitiesSection = document.querySelector('.universities');
-
-
-const scrollSpeed = 4; 
-
-function scrollUniversities() {
-  universitiesSection.scrollLeft += scrollSpeed;
-  
-  
-  if (universitiesSection.scrollLeft >= universitiesSection.scrollWidth - universitiesSection.offsetWidth) {
-    const imgElements = universitiesSection.querySelectorAll('img');
-    imgElements.forEach(img => {
-      const cloneImg = img.cloneNode(true);
-      universitiesSection.appendChild(cloneImg);
-    });
-  }
-}
-
-const scrollInterval = setInterval(scrollUniversities, 50); 
-
-
-universitiesSection.addEventListener('mouseenter', function () {
-  clearInterval(scrollInterval);
-});
-
-
-universitiesSection.addEventListener('mouseleave', function () {
-  scrollInterval = setInterval(scrollUniversities, 50);
-});
-});
-
-$(document).ready(function(){
-$(".front_arrow").click(function(){
-  $("#testimonial-carousel").carousel("next");
-});
-
-$(".back_arrow").click(function(){
-  $("#testimonial-carousel").carousel("prev");
-});
-
-var cardsInSlide = 3;
-var totalCards = $(".carousel-cards").length;
-
-$("#testimonial-carousel").on("slide.bs.carousel", function (event) {
-  var index = $(event.relatedTarget).index();
-  var visibleCards = cardsInSlide;
-
-  if (index + cardsInSlide > totalCards) {
-    visibleCards = totalCards - index;
-  }
-
-  $(".carousel-cards").removeClass("active");
-  $(".carousel-cards").slice(index, index + visibleCards).addClass("active");
-});
-});
-
-// function openPopup() {
-//   document.getElementById('form').style.display = 'flex';
-// }
-// function closePopup() {
-//   document.getElementById('form').style.display = 'none';
-// }
-
-document.addEventListener('DOMContentLoaded', function() {
   var sets = document.querySelectorAll('.content-set');
   var nextButton = document.getElementById('next');
   var bookButton = document.getElementById('book');
@@ -88,28 +223,29 @@ document.addEventListener('DOMContentLoaded', function() {
   var currentSet = 0;
 
   function openPopup() {
-      document.getElementById('popupForm').style.display = 'block';
-      resetForm();
+    document.getElementById('popupForm').style.display = 'block';
+    resetForm();
   }
 
   window.openPopup = openPopup; // Make it globally accessible if referenced by 'onclick' attributes
 
   function closePopup() {
-      document.getElementById('popupForm').style.display = 'none';
+    document.getElementById('popupForm').style.display = 'none';
   }
 
   function toggleContent() {
-      sets[currentSet].style.display = 'none';
-      currentSet++;
-      sets[currentSet].style.display = 'block';
-      
-      if (currentSet === sets.length - 1) {
-          nextButton.style.display = 'none';
-          bookButton.style.display = 'block';
-      }
+    sets[currentSet].style.display = 'none';
+    currentSet++;
+    sets[currentSet].style.display = 'block';
+
+    if (currentSet === sets.length - 1) {
+      nextButton.style.display = 'none';
+      bookButton.style.display = 'block';
+    }
   }
 
   window.toggleContent = toggleContent;
+
   function submitForm() {
     // Hide form container
     document.getElementById('formContainer').style.display = 'none';
@@ -117,33 +253,33 @@ document.addEventListener('DOMContentLoaded', function() {
     // Show tick and hide popup after 2 seconds
     tickContainer.style.display = 'flex';
 
-    setTimeout(function() {
-        tickContainer.style.display = 'none'; // Hide the tick container
-        resetForm();
-        closePopup(); // This should come last after the tick disappears
+    setTimeout(function () {
+      tickContainer.style.display = 'none'; // Hide the tick container
+      resetForm();
+      closePopup(); // This should come last after the tick disappears
     }, 2000);
-}
+  }
 
   window.submitForm = submitForm;
 
   function resetForm() {
-      document.getElementById('consultationForm').reset();
-      sets.forEach(function(set, index) {
-          set.style.display = index === 0 ? 'block' : 'none';
-      });
-      currentSet = 0;
-      
-      // Reset buttons display
-      nextButton.style.display = 'block';
-      bookButton.style.display = 'none';
-      
-      // Ensure the form container is displayed when resetting
-      document.getElementById('formContainer').style.display = 'block';
+    document.getElementById('consultationForm').reset();
+    sets.forEach(function (set, index) {
+      set.style.display = index === 0 ? 'block' : 'none';
+    });
+    currentSet = 0;
+
+    // Reset buttons display
+    nextButton.style.display = 'block';
+    bookButton.style.display = 'none';
+
+    // Ensure the form container is displayed when resetting
+    document.getElementById('formContainer').style.display = 'block';
   }
 
   window.resetForm = resetForm;
 
-  closeButton.addEventListener('click', function() {
-      closePopup();
+  closeButton.addEventListener('click', function () {
+    closePopup();
   });
 });
